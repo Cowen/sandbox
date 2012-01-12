1f6970c829bcb1b76ddbab8e0b74fcd3f751f839
Conrad Owen <teknobo@gmail.com>
2012-01-12 17:06:47 -0500

    Update iPack.py

--- iPack.py
+++ iPack.py
@@ -12,29 +12,32 @@ def sanitize(path):
     
     Super ugly, would refactor it if I ever needed to change it. But I don't, and it works.
     """
-    return urllib.url2pathname('/' + path.lstrip().lstrip("<key>Location</key><string>file://localhost").rstrip().rstrip("</string>")).replace("&#38;", '&')
+    return urllib.url2pathname(
+                '/' + path.lstrip().lstrip("<key>Location</key><string>file://localhost")
+                .rstrip().rstrip("</string>"))
+                .replace("&#38;", '&')
 
 def pack(lib_xml, out_name):
     """
-    Packs the iTunes Library XML file specified by lib_xml into a .tar.gz file named out_name.
+    Pack the music files in iTunes XML file (lib_xml) into a .tar.gz file (out_name).
     """
     xml = open(lib_xml)
+    
     print 'Parsing ', lib_xml
-
     mp3s = []
     for line in xml:
         if "<key>Location</key><string>" in line:
             mp3s.append(sanitize(line))
     xml.close()
 
-    tar = tarfile.open(out_name, 'w:gz')
     print 'Compressing',lib_xml, 'to', out_name
+    tar = tarfile.open(out_name, 'w:gz')
     for mp3 in mp3s:
         print mp3.rpartition('/')[2]
         try:
             tar.add(mp3)
         except:
-            print 'Exception!'
+            print "Unexpected error:", sys.exc_info()[0]
             tar.close()
     tar.close()
 
